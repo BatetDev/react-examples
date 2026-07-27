e38cb88b01d3c90b9fe1a2266be09f71bab29f1c
fr4nbtt <batet.francisco@gmail.com>
2026-07-27 18:38:21 -0300

    Add color change counter and fix layout

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -1,24 +1,17 @@
 import { useState } from 'react';
 import './App.css';
 
-const COLORS = [
-  'pink',
-  'green',
-  'blue',
-  'yellow',
-  'purple',
-  'orange',
-  'black',
-  'red',
-  'silver',
-  'teal',
-];
+const COLORS = ['green', 'blue', 'purple', 'red'];
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [count, setCount] = useState(0);
 
   const onButtonClick = (color) => () => {
-    setBackgroundColor(color);
+    if (color !== backgroundColor) {
+      setBackgroundColor(color);
+      setCount(count + 1);
+    }
   };
 
   return (
@@ -28,6 +21,8 @@ function App() {
         backgroundColor,
       }}
     >
+      <h1>BG color change count: {count}</h1>
+
       {COLORS.map((color) => (
         <button
           type="button"
